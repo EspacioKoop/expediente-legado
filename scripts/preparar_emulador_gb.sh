67cc862720c71d5047fa52e07fe4f57d013610c3
eGurucharri <140599479+eGurucharri@users.noreply.github.com>
2026-09-13 21:14:32 +0200

    build(gb): reactiva el perfil mínimo de bindings

--- scripts/preparar_emulador_gb.sh
+++ scripts/preparar_emulador_gb.sh
@@ -73,6 +73,7 @@ compilar_nativo() {
             platform="$plataforma" \
             target="$objetivo" \
             arch=x86_64 \
+            build_profile=build_profile.json \
             godot_cpp_dir="$GODOT_CPP" \
             peanut_gb_dir="$PEANUT"
     )
