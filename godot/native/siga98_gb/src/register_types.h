5bc08bcb70c5ce631d6934eec3fd8002acecc579
eGurucharri <140599479+eGurucharri@users.noreply.github.com>
2026-09-13 20:57:09 +0200

    feat(gb): declara registro de la GDExtension

---
+++ godot/native/siga98_gb/src/register_types.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <godot_cpp/core/class_db.hpp>
+
+void initialize_siga98_gb_module(godot::ModuleInitializationLevel p_level);
+void uninitialize_siga98_gb_module(godot::ModuleInitializationLevel p_level);
