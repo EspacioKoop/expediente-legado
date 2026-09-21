7eced438fd6fa2dfff66748984ec7450182f9eca
eGurucharri <140599479+eGurucharri@users.noreply.github.com>
2026-09-22 00:23:15 +0200

    fix(jungian): configure shadow archetype

--- godot/arquetipos/sombra.gd
+++ godot/arquetipos/sombra.gd
@@ -1,4 +1,11 @@
 extends "res://arquetipos/arquetipo_base.gd"
 
+
 func _init() -> void:
-    ._init("sombra", "Sombra", "Aspecto reprimido que emerge en combate", {"bonus_crit": 0.15, "bonus_daño_sombra": 0.2}, 150)
+	configurar(
+		"sombra",
+		"Sombra",
+		"Aspecto reprimido que emerge en combate",
+		{"bonus_crit": 0.15, "bonus_dano_sombra": 0.2},
+		150
+	)
