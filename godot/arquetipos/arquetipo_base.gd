2805b91b737adb01081c117a6cadc5873a5d6950
eGurucharri <140599479+eGurucharri@users.noreply.github.com>
2026-09-22 00:23:13 +0200

    fix(jungian): make archetype base Godot 4 safe

--- godot/arquetipos/arquetipo_base.gd
+++ godot/arquetipos/arquetipo_base.gd
@@ -1,26 +1,36 @@
 extends Node
 
-# Arquetipo Jungiano base - se desbloquea via interacción documental
-signal activado
+signal activado(arquetipo_id: String)
 
-var id: String
-var nombre: String
-var descripcion: String
-var efecto_combate: Dictionary
+var id: String = ""
+var nombre: String = ""
+var descripcion: String = ""
+var efecto_combate: Dictionary = {}
 var desbloqueado: bool = false
 var puntos_insight_requeridos: int = 100
 
-func _init(p_id: str, p_nombre: str, p_descripcion: str, p_efecto: Dictionary, p_puntos: int) -> void:
-    id = p_id
-    nombre = p_nombre
-    descripcion = p_descripcion
-    efecto_combate = p_efecto
-    puntos_insight_requeridos = p_puntos
 
-func desbloquear() -> void:
-    desbloqueado = true
-    activado.emit()
-    print("Arquetipo %s desbloqueado: %s" % [nombre, efecto_combate])
+func configurar(
+	p_id: String,
+	p_nombre: String,
+	p_descripcion: String,
+	p_efecto: Dictionary,
+	p_puntos: int
+) -> void:
+	id = p_id
+	nombre = p_nombre
+	descripcion = p_descripcion
+	efecto_combate = p_efecto.duplicate(true)
+	puntos_insight_requeridos = maxi(0, p_puntos)
+
+
+func desbloquear() -> bool:
+	if desbloqueado:
+		return false
+	desbloqueado = true
+	activado.emit(id)
+	return true
+
 
 func obtener_efecto() -> Dictionary:
-    return efecto_combate if desbloqueado else {}
+	return efecto_combate.duplicate(true) if desbloqueado else {}
