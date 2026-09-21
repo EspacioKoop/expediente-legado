788d98f9517d9e17f0db887cde4aa9cca6aac3a8
eGurucharri <140599479+eGurucharri@users.noreply.github.com>
2026-09-22 00:48:19 +0200

    fix(godot): make literary integration valid GDScript 4

--- godot/interactables/libros/libro_interactivo.gd
+++ godot/interactables/libros/libro_interactivo.gd
@@ -1,49 +1,57 @@
 extends Area3D
 
 @onready var label = $InteractionLabel
-@export var obra_id: String = "odisea"  # default
+@export var obra_id: String = "odisea"
+
 
 func _ready() -> void:
-    label.visible = false
+	label.visible = false
+
 
 func _on_area_entered(area: Area3D) -> void:
-    if area.is_in_group("jugador"):
-        label.visible = true
+	if area.is_in_group("jugador"):
+		label.visible = true
+
 
 func _on_area_exited(area: Area3D) -> void:
-    if area.is_in_group("jugador"):
-        label.visible = false
+	if area.is_in_group("jugador"):
+		label.visible = false
 
-func _input_event(viewport: Viewport, event: InputEvent, shape_idx: int) -> void:
-    if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
-        _interactuar()
-    if event is InputEventKey and event.pressed and event.scancode == KEY_E:
-        _interactuar()
+
+func _input_event(_viewport: Viewport, event: InputEvent, _shape_idx: int) -> void:
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		_interactuar()
+	if event is InputEventKey and event.pressed and event.keycode == KEY_E:
+		_interactuar()
+
 
 func _interactuar() -> void:
-    var gestor = GestorLiteratura
-    if gestor.conocer_obra(obra_id):
-        label.text = f"Has descubierto: {obra_id}"
-        # otorgar insight adicional basado en efecto de obra
-        var efecto = _obtener_efecto_obra(obra_id)
-        if efecto.has("bonus_insight"):
-            GestorArquetipos.ganar_insight(int(efecto.bonus_insight))
-        # reproducir sonido, particulas, etc.
-        print(f"Interactuado con obra {obra_id}")
-    else:
-        label.text = "Ya conoces esta obra"
-    yield(get_tree().create_timer(2.0), "timeout")
-    label.text = "E para leer"
+	var gestor = GestorLiteratura
+	if gestor.conocer_obra(obra_id):
+		label.text = "Has descubierto: %s" % obra_id
+		var efecto := _obtener_efecto_obra(obra_id)
+		if efecto.has("bonus_insight"):
+			GestorArquetipos.ganar_insight(int(efecto.get("bonus_insight", 0)))
+		print("Interactuado con obra %s" % obra_id)
+	else:
+		label.text = "Ya conoces esta obra"
+	await get_tree().create_timer(2.0).timeout
+	label.text = "E para leer"
 
-func _obtener_efecto_obra(obra_id: String) -> Dictionary:
-    # carga datos desde JSON (simple version)
-    var file = FileAccess.open("res://datos/literatura/obras.json", FileAccess.READ)
-    if file:
-        var data = JSON.parse_string(file.get_as_text())
-        if data.error == OK:
-            for o in data.obras:
-                if o.id == obra_id:
-                    file.close()
-                    return o.efecto
-        file.close()
-    return {}
+
+func _obtener_efecto_obra(id_obra: String) -> Dictionary:
+	var archivo := FileAccess.open("res://datos/literatura/obras.json", FileAccess.READ)
+	if archivo == null:
+		return {}
+	var data = JSON.parse_string(archivo.get_as_text())
+	archivo.close()
+	if not (data is Dictionary):
+		return {}
+	var obras = data.get("obras", [])
+	if not (obras is Array):
+		return {}
+	for obra in obras:
+		if obra is Dictionary and String(obra.get("id", "")) == id_obra:
+			var efecto = obra.get("efecto", {})
+			return efecto if efecto is Dictionary else {}
+	return {}
