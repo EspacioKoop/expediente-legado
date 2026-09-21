5685f93fbc29d19f2791252119f8199bf50b01b8
eGurucharri <140599479+eGurucharri@users.noreply.github.com>
2026-09-22 00:33:36 +0200

    fix(jungian): make headless scripts autoload-safe

--- godot/test_jungian/test_jungian.gd
+++ godot/test_jungian/test_jungian.gd
@@ -2,10 +2,16 @@ extends Node3D
 
 
 func _ready() -> void:
-	GestorArquetipos.reiniciar()
-	GestorMomentum.reiniciar()
-	GestorCombos.reiniciar()
-	GestorArquetipos.ganar_insight(350)
+	var arquetipos := get_node_or_null("/root/GestorArquetipos")
+	var momentum := get_node_or_null("/root/GestorMomentum")
+	var combos := get_node_or_null("/root/GestorCombos")
+	if arquetipos != null:
+		arquetipos.call("reiniciar")
+		arquetipos.call("ganar_insight", 350)
+	if momentum != null:
+		momentum.call("reiniciar")
+	if combos != null:
+		combos.call("reiniciar")
 
 	var combate := JuicioCombate3D.new()
 	combate.name = "ArenaJungianaPrueba"
