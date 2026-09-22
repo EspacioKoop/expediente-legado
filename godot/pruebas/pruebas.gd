195b68ef5972664e5550de6e13cdd987dbc767f2
eGurucharri <140599479+eGurucharri@users.noreply.github.com>
2026-09-22 17:55:49 +0200

    test(juicio): integrar feedback en suite canónica

--- godot/pruebas/pruebas.gd
+++ godot/pruebas/pruebas.gd
@@ -33,6 +33,7 @@ func _init() -> void:
 	PruebasPrometeoYCombate._jornada(comprobar_cb)
 	PruebasJungian.todo(comprobar_cb)
 	PruebasJuicioReglas.todo(comprobar_cb)
+	PruebasJuicioFeedback.todo(comprobar_cb)
 	PruebasAlquiler.todo(comprobar_cb)
 	PruebasImprevistos.todo(comprobar_cb)
 	PruebasPrometeoYCombate._procedencia(comprobar_cb)
