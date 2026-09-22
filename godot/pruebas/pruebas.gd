6878a7cb166a558bfacd99d98e100bba7a9aa44b
eGurucharri <140599479+eGurucharri@users.noreply.github.com>
2026-09-22 18:39:25 +0200

    test: añadir regresiones del rival a la suite

--- godot/pruebas/pruebas.gd
+++ godot/pruebas/pruebas.gd
@@ -33,6 +33,7 @@ func _init() -> void:
 	PruebasPrometeoYCombate._jornada(comprobar_cb)
 	PruebasJungian.todo(comprobar_cb, root)
 	PruebasJuicioReglas.todo(comprobar_cb)
+	PruebasJuicioRival.todo(comprobar_cb)
 	PruebasJuicioFeedback.todo(comprobar_cb)
 	PruebasJuicioJungiano.todo(comprobar_cb)
 	PruebasJuicioSimbolico.todo(comprobar_cb)
